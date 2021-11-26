b4818b1dfe2aabdf8a76dd1e7d9c5cce38944397
KarelSpil <Karel.Spil@gmail.com>
2021-11-26 16:50:32 +0200

    add functionality to buttons
    add Start button functionality
    add Quit button functionality

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuUIHandler : MonoBehaviour
 {
@@ -15,4 +19,16 @@ public class MenuUIHandler : MonoBehaviour
     // {
         
     // }
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
 }
